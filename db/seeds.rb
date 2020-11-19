258259d41515dcaf64d3f8a1ac16dec6bbf1a967
Miguel Pimenta <jmiguelpimenta@gmail.com>
2020-11-19 15:36:12 -0300

    Added models migrtae and seed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# require 'open-uri'
+require 'json'
+require 'net/http'
+
+# URL to json ingredients
+url = URI('https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list')
+api_call = Net::HTTP.get(url)
+ingredients = JSON.parse(api_call)['drinks']
+ingredients.each do |ingredient|
+  ing = ingredient['strIngredient1']
+  Ingredient.create!(name: ing)
+end
