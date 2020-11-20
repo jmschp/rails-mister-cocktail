7793a7201a53171791b795c4cce88cb72e00ee9d
Miguel Pimenta <jmiguelpimenta@gmail.com>
2020-11-20 18:59:48 -0300

    Removed column from bd cocktail

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_20_193006) do
+ActiveRecord::Schema.define(version: 2020_11_20_194703) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,7 +40,6 @@ ActiveRecord::Schema.define(version: 2020_11_20_193006) do
     t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "image_url", null: false
   end
 
   create_table "doses", force: :cascade do |t|
